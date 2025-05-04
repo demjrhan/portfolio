d256b8826ecb945ac619072ea1c496eff5fb8f40
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-05-05 00:46:34 +0200

    hide navbar and added footer

---
+++ frontend/public/scripts/landingText.js
@@ -0,0 +1,38 @@
+﻿const languageList = [
+    'Hello, x!'
+];
+
+const keywordList = [
+    'Programmer',
+    'World',
+    'Developer',
+    'Coder',
+    'Recruiter',
+    'Friend'
+];
+
+const gradientColors = [
+    'hsla(120, 40%, 30%, 1)',
+    'hsla(30, 60%, 40%, 1)',
+    'hsla(200, 70%, 35%, 1)',
+    'hsla(90, 50%, 35%, 1)',
+    'hsla(20, 70%, 30%, 1)',
+    'hsla(160, 40%, 30%, 1)',
+    'hsla(220, 50%, 35%, 1)'
+];
+
+export function initLandingText() {
+    const landing = document.getElementById('landing');
+    const languageIndex = Math.floor(Math.random() * languageList.length);
+    const baseSyntax = languageList[languageIndex];
+    let keywordIndex = 0;
+
+    landing.innerHTML = "Welcome to my portfolio!";
+
+    setInterval(() => {
+        const randomColor = gradientColors[Math.floor(Math.random() * gradientColors.length)];
+        const coloredKeyword = `<span class="animated-keyword" style="color: ${randomColor}; font-family: 'Pixelify Sans', sans-serif">${keywordList[keywordIndex]}</span>`;
+        landing.innerHTML = baseSyntax.replace("x", coloredKeyword);
+        keywordIndex = (keywordIndex + 1) % keywordList.length;
+    }, 2500);
+}
---
+++ frontend/public/scripts/navbarObserver.js
@@ -0,0 +1,33 @@
+﻿function isMobileView() {
+    return window.innerWidth <= 768;
+}
+export function setupNavbarObserver() {
+    //const navbar = document.querySelector('.navbar');
+    const landingSection = document.getElementById('landing');
+    const content = document.querySelector('.content');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (!isMobileView()) {
+                if (entry.isIntersecting) {
+                    //navbar.classList.add('hidden');
+                    //navbar.style.opacity = '0';
+                    content.style.marginLeft = '0px';
+                    content.style.filter = 'blur(3px)';
+                } else {
+                    //navbar.classList.remove('hidden');
+                    //navbar.style.opacity = '1';
+                    //content.style.marginLeft = '250px';
+                    content.style.filter = 'blur(0px)';
+                }
+            } else {
+                content.style.marginLeft = '0px';
+                content.style.filter = entry.isIntersecting ? 'blur(3px)' : 'blur(0px)';
+            }
+        });
+    }, {
+        threshold: 0.8
+    });
+
+    observer.observe(landingSection);
+}
