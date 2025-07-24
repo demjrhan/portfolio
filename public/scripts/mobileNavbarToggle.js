40e49672b8cc14ccb17bd206fe5ce7e4cfe6e99a
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-07-24 12:46:38 +0200

    inital start

--- public/scripts/mobileNavbarToggle.js
+++
@@ -1,9 +0,0 @@
-﻿export function setupNavbarToggle() {
-    const navbarToggle = document.querySelector('.navbar-toggle');
-    const navbar = document.querySelector('.navbar');
-
-    navbarToggle.addEventListener('click', () => {
-        navbar.classList.toggle('show');
-        document.body.classList.toggle('mobile-nav-open');
-    });
-}
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            {/* We'll add Nav, Hero, Projects, Footer here */}
+        </div>
+    );
+}
