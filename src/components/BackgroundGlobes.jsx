82d683e60e622e1191a00d663606fe28425d057f
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-08-04 21:43:04 +0200

    updated background blobes. it was also too heavy for mobile

--- src/components/BackgroundGlobes.jsx
+++ src/components/BackgroundGlobes.jsx
@@ -1,69 +1,20 @@
-import { motion } from "framer-motion";
-import React from "react";
-
 export default function BackgroundBlobs() {
-    const transition = {
-        repeat: Infinity,
-        repeatType: "mirror",
-        ease: "easeInOut",
-    };
-
     return (
         <div className="absolute inset-0 -z-10 overflow-hidden">
             {/* Top-Left Blob */}
-            <motion.div
-                className="absolute top-[5%] left-[5%] w-[350px] h-[350px] bg-gray-700 rounded-full filter blur-[120px] opacity-35 mix-blend-multiply"
-                animate={{
-                    x: [0, 40, 60, -30, -50, 0],
-                    y: [0, -30, 40, 50, -20, 0],
-                    scale: [1, 1.05, 0.95, 1.1, 1],
-                }}
-                transition={{ ...transition, duration: 26 }}
-            />
-
-            {/* Top-Right Blob */}
-            <motion.div
-                className="absolute top-[5%] right-[5%] w-[400px] h-[400px] bg-gray-600 rounded-full filter blur-[130px] opacity-30 mix-blend-multiply"
-                animate={{
-                    x: [0, -50, -70, 40, 20, 0],
-                    y: [0, -40, 20, 60, -30, 0],
-                    scale: [1, 1.1, 1, 0.9, 1.05, 1],
-                }}
-                transition={{ ...transition, duration: 28 }}
-            />
-
-            {/* Bottom-Left Blob */}
-            <motion.div
-                className="absolute bottom-[5%] left-[5%] w-[400px] h-[400px] bg-black rounded-full filter blur-[140px] opacity-30 mix-blend-multiply"
-                animate={{
-                    x: [0, 60, 30, -50, -20, 0],
-                    y: [0, 50, 70, -40, -20, 0],
-                    scale: [1, 1.05, 1.1, 0.95, 1, 1],
-                }}
-                transition={{ ...transition, duration: 30 }}
-            />
+            <div className="blob bg-gray-700 top-[10%] left-[10%] scale-[2]" />
 
             {/* Bottom-Right Blob */}
-            <motion.div
-                className="absolute bottom-[5%] right-[5%] w-[350px] h-[350px] bg-gray-800 rounded-full filter blur-[120px] opacity-35 mix-blend-multiply"
-                animate={{
-                    x: [0, -40, -60, 30, 50, 0],
-                    y: [0, 60, 40, -50, -30, 0],
-                    scale: [1, 1.1, 0.9, 1.05, 1],
-                }}
-                transition={{ ...transition, duration: 32 }}
-            />
+            <div className="blob bg-gray-800 bottom-[10%] right-[10%] scale-[2]" />
 
-            {/* Middle Blob */}
-            <motion.div
-                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-gray-500 rounded-full filter blur-[160px] opacity-25 mix-blend-multiply"
-                animate={{
-                    x: [0, 30, -40, 50, -30, 0],
-                    y: [0, -20, 30, -40, 20, 0],
-                    scale: [1, 1.1, 1, 0.95, 1.05, 1],
-                }}
-                transition={{ ...transition, duration: 34 }}
-            />
+            {/* Center Blob */}
+            <div className="blob bg-gray-500 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-[2.5]" />
+
+            {/* Top-Right Blob */}
+            <div className="blob bg-gray-600 top-[5%] right-[15%] scale-[2]" />
+
+            {/* Bottom-Left Blob */}
+            <div className="blob bg-gray-900 bottom-[5%] left-[20%] scale-[2]" />
         </div>
     );
 }
